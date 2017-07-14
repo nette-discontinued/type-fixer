fc99a115dcb8e58a434968712bfda9fc9bc47402
David Grudl <david@grudl.com>
2017-07-14 13:54:51 +0200

    fixed typehint

--- src/Collector.php
+++ src/Collector.php
@@ -27,7 +27,7 @@ final class Collector
 	/**
 	 * @return ParserReflection\ReflectionClass[]
 	 */
-	public function collect(\Traversable $iterator): array
+	public function collect(iterable $iterator): array
 	{
 		$this->parseFiles($iterator);
 		$this->completeClasses();
